2f3579847af99cc23dd5f8fd8e47a9ee146bda2c
Vuyani Ncube <nvuyani307@gmail.com>
2022-12-20 22:12:10 +0200

    semi complete website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import {IvyCarouselModule} from 'angular-responsive-carousel';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    IvyCarouselModule
+    IvyCarouselModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,133 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { trigger, style, animate, transition, query, keyframes, group } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.sass'],
+  animations: [
+
+    trigger('burgerMenu', [
+
+      transition('straight => angled',[
+
+        group([
+
+        query('#line1', [
+            animate('700ms  ease-out', keyframes([
+              style({ transform: "translateY(0px)", offset: 0 }),
+              style({ transform: "translateY(8px)", offset: 0.5}),
+              style({ transform: "translate(0px, 8px) rotate(45deg)", offset: 1})
+            ]))
+          ]
+        ),
+
+        query('#line2', [
+            animate('350ms ease-out', keyframes([
+              style({ opacity: 1, offset: 0}),
+              style({ opacity: 0, offset: 1})
+            ]))
+        ]
+        ),
+
+        query('#line3',
+            animate('700ms ease-out', keyframes([
+              style({ transform: "translateY(0px)", offset: 0 }),
+              style({ transform: "translateY(-8px)", offset: 0.5}),
+              style({ transform: "translate(0px, -8px) rotate(-45deg)", offset: 1})
+            ]))
+        ),
+
+        query('#glass',
+          animate('350ms ease-out', keyframes([
+            style({ transform: 'translateX(400px)', offset: 0 }),
+            style({ transform: 'translateX(200px)', offset: 0.5 }),
+            style({ transform: 'translateX(0px)', offset: 1}),
+          ]))
+        ),
+
+        ])
+
+      ]),
+
+      transition('angled => straight',[
+
+        group([
+
+        query('#line1', [
+            animate('700ms  ease-out', keyframes([
+              style({ transform: "translateY(8px)", offset: 0 }),
+              style({ transform: "translateY(0px)", offset: 0.5}),
+              style({ transform: "translate(0px, 0px) rotate(0deg)", offset: 1})
+            ]))
+          ]
+        ),
+
+        query('#line2', [
+            animate('350ms ease-out', keyframes([
+              style({ opacity: 0, offset: 0}),
+              style({ opacity: 1, offset: 1})
+            ]))
+        ]
+        ),
+
+        query('#line3',
+            animate('700ms ease-out', keyframes([
+              style({ transform: "translateY(-8px)", offset: 0 }),
+              style({ transform: "translateY(0px)", offset: 0.5}),
+              style({ transform: "translate(0px, 0px) rotate(0deg)", offset: 1})
+            ]))
+        ),
+
+        query('#glass',
+          animate('500ms ease-out', keyframes([
+            style({ transform: 'translateX(0px)', offset: 0 }),
+            style({ transform: 'translateX(200px)', offset: 0.5 }),
+            style({ transform: 'translateX(400px)', offset: 1}),
+          ]))
+        ),
+
+        ])
+
+      ])
+
+    ])
+  ]
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  color : string = '';
+  state: string = 'straight';
+  straight: boolean = true;
+  display: string = 'none';
+
+  constructor( private route: Router) {}
+
+  ngOnInit(): void {
+
+    this.route.events.subscribe(event => {
+      console.log(event)
+      this.state = 'straight';
+      this.straight = true;
+      this.display = 'none'
+    })
+  }
+
+  //function opens and closes menu
+  openClose() {
+
+    if (this.state === 'straight') {
+      this.state = 'angled';
+      this.straight = false;
+      this.display = 'block';
+    }
+
+    else{
+      this.state = 'straight';
+      this.straight = true;
+      this.display = 'none';
+    }
+  }
 
 }
